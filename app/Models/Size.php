dc3cd0fb2145ac401cacdd436380179ab4787ff6
eferreyro <emafe7472@gmail.com>
2021-11-01 23:30:00 -0600

    Agregando tablas iniciales para productos

--- app/Models/Size.php
+++ app/Models/Size.php
@@ -5,10 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Category extends Model
+class Size extends Model
 {
     use HasFactory;
-
-    //Asignacion maciva para el modelo
-    protected $fillable = ['name', 'slug', 'image', 'icon'];
 }
